1605910b296d851c165a6e638c2ad80c995590e2
Husnul M <husnulmub@gmail.com>
2022-09-13 21:53:45 +0700

    update

--- resources/views/transaction/document/v2/index.blade.php
+++ resources/views/transaction/document/v2/index.blade.php
@@ -177,7 +177,8 @@
 @section('additional-js')
 <script src="{{ asset('/assets/ckeditor/ckeditor.js') }}"></script>
 <script src="{{ asset('/assets/ckeditor/adapters/jquery.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="{{ asset('/assets/js/select2.min.js') }}"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->
 <script type="text/javascript">
     $(document).ready(function () {        
         var count = 0;
