43cd9b6c9b4223f4dab1a56bae97e4654256c2a2
Husnul M <husnulmub@gmail.com>
2022-09-14 14:42:09 +0700

    update

--- resources/views/transaction/document/documentdetail.blade.php
+++ resources/views/transaction/document/documentdetail.blade.php
@@ -500,7 +500,7 @@
 
 @section('additional-js')
 <script src="{{ asset('/assets/ckeditor/ckeditor.js') }}"></script>
-<script src="{{ asset('/assets/ckeditor/adapters/jquery.js') }}"></script>
+<!-- <script src="{{ asset('/assets/ckeditor/adapters/jquery.js') }}"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script type="text/javascript">
 
