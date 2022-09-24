7052bd402bdcc449084edfd514f74ac64ce27f60
Husnul M <husnulmub@gmail.com>
2022-09-24 21:10:25 +0700

    update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,10 +13,20 @@
   <link rel="stylesheet" href="{{ ('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ ('assets/dist/css/adminlte.min.css') }}">
+
+  <style>
+    .loginbody 
+    {
+      background-image: url("assets/img/login-bg.JPEG");
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+  </style>
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
+<body class="hold-transition login-page loginbody">
+<div class="login-box" style="margin-right:1000px; margin-bottom: 10rem;">
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
       <a href="#" class="h5"><b>Document Management System</b></a>
@@ -43,44 +53,14 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-8">
-            <!-- <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div> -->
-          </div>
-          <!-- /.col -->
           <div class="col-4">
             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
           </div>
-          <!-- /.col -->
         </div>
       </form>
-
-      <!-- <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div> -->
-      <!-- /.social-auth-links -->
-
-      <!-- <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p> -->
     </div>
-    <!-- /.card-body -->
   </div>
-  <!-- /.card -->
 </div>
-<!-- /.login-box -->
 
 <!-- jQuery -->
 <script src="{{ ('assets/plugins/jquery/jquery.min.js') }}"></script>
